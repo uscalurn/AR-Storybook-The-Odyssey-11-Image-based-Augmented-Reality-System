996bf90a72336ca54a32652de4cf29d4b718e287
Minh Nhan Nguyen <110551743+uscalurn@users.noreply.github.com>
2026-04-22 21:46:32 +0700

    Update DemonAttack.cs

--- Scripts/DemonAttack.cs
+++ Scripts/DemonAttack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class demontancong : MonoBehaviour
+public class DemonAttack : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
